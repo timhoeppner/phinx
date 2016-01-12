62bbd3c1cd97c40ae23f4eb23337d147068d06e0
Daniel Gomes <daniel.gomes@sociomantic.com>
2016-01-12 18:36:56 +0100

    Migrate and Rollback commands: removed shortcut for --dry-run option

--- src/Phinx/Console/Command/Rollback.php
+++ src/Phinx/Console/Command/Rollback.php
@@ -47,7 +47,7 @@ class Rollback extends AbstractCommand
              ->setDescription('Rollback the last or to a specific migration')
              ->addOption('--target', '-t', InputOption::VALUE_REQUIRED, 'The version number to rollback to')
              ->addOption('--date', '-d', InputOption::VALUE_REQUIRED, 'The date to rollback to')
-             ->addOption('--dry-run', '-r', InputOption::VALUE_NONE, 'Output SQL rather than executing it')
+             ->addOption('--dry-run', null, InputOption::VALUE_NONE, 'Output SQL rather than executing it')
              ->setHelp(
 <<<EOT
 The <info>rollback</info> command reverts the last migration, or optionally up to a specific version
